19b845d2aa41c67dc7930c27aefe7071369a2a42
Siren <siren@kernal.eu>
2021-12-28 07:02:08 -0500

    Make sure callback interval values aren't shorter than 30 seconds

--- internal/moneropayd/config/config.go
+++ internal/moneropayd/config/config.go
@@ -69,7 +69,11 @@ func Init() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		// TODO: Make sure no values less than 30sec or however long is the thread interval time.
+
+		if v < 30 * time.Second {
+			log.Println("Callback interval cannot be shorter than 30s, defaulting to 30s.")
+			v, _ = time.ParseDuration("30s")
+		}
 		Values.Intervals = append(Values.Intervals, v)
 	}
 }

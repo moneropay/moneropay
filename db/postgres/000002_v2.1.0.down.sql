7f387f190a591c8bcd6828cb23d4a70dfc54bed8
Siren <siren@kernal.eu>
2022-06-16 01:07:28 +0300

    Remove if statements

--- db/postgres/000002_v2.1.0.down.sql
+++ db/postgres/000002_v2.1.0.down.sql
@@ -3,25 +3,7 @@ CREATE TABLE IF NOT EXISTS metadata (
 	key text UNIQUE NOT NULL,
 	value bigint NOT NULL
 );
-DO $$
-BEGIN
-    IF EXISTS
-        (SELECT 1 FROM information_schema.tables WHERE table_name = 'last_block_height')
-    THEN
-        INSERT INTO metadata (key, value) VALUES ('last_height', (SELECT height FROM last_block_height LIMIT 1));
-    ELSE
-        INSERT INTO metadata (key, value) VALUES ('last_height', 0);
-    END IF;
-END;
-$$;
+INSERT INTO metadata (key, value) SELECT 'last_height', (SELECT height FROM last_block_height LIMIT 1) WHERE NOT EXISTS (SELECT 1 FROM metadata);
 DROP TABLE IF EXISTS last_block_height;
-DO $$
-BEGIN
-    IF EXISTS
-        (SELECT 1 FROM information_schema.tables WHERE table_name = 'subaddresses')
-    THEN
-	ALTER TABLE subaddresses RENAME COLUMN address_index TO index;
-    END IF;
-END;
-$$;
+ALTER TABLE subaddresses RENAME COLUMN address_index TO index;
 COMMIT;
--- db/postgres/000002_v2.1.0.up.sql
+++ db/postgres/000002_v2.1.0.up.sql
@@ -2,25 +2,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS last_block_height (
 	height bigint NOT NULL DEFAULT 0
 );
-DO $$
-BEGIN
-    IF EXISTS
-        (SELECT 1 FROM information_schema.tables WHERE table_name = 'metadata')
-    THEN
-        INSERT INTO last_block_height (height) VALUES ((SELECT value FROM metadata WHERE key = 'last_height'));
-    ELSE
-        INSERT INTO last_block_height (height) VALUES (0);
-    END IF;
-END;
-$$;
+INSERT INTO last_block_height (height) SELECT (SELECT value FROM metadata WHERE key = 'last_height') WHERE NOT EXISTS (SELECT 1 FROM last_block_height);
 DROP TABLE IF EXISTS metadata;
-DO $$
-BEGIN
-    IF EXISTS
-        (SELECT 1 FROM information_schema.tables WHERE table_name = 'subaddresses')
-    THEN
-	ALTER TABLE subaddresses RENAME COLUMN index TO address_index;
-    END IF;
-END;
-$$;
+ALTER TABLE subaddresses RENAME COLUMN index TO address_index;
 COMMIT;
